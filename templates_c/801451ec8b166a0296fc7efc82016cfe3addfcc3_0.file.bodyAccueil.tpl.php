872a8f33645a097699c5571bffb0ae551f10e6b4
Firyo <56397435+Firyo@users.noreply.github.com>
2021-02-27 15:07:17 +0100

    Changed font-size for h1 and h2

--- templates_c/801451ec8b166a0296fc7efc82016cfe3addfcc3_0.file.bodyAccueil.tpl.php
+++ templates_c/801451ec8b166a0296fc7efc82016cfe3addfcc3_0.file.bodyAccueil.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.38, created on 2021-02-27 14:01:40
+/* Smarty version 3.1.38, created on 2021-02-27 14:03:23
   from 'D:\Programmes\laragon\www\lbdntools\templates\bodyAccueil.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.38',
-  'unifunc' => 'content_603a50c4124401_97905731',
+  'unifunc' => 'content_603a512b322136_61938345',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '801451ec8b166a0296fc7efc82016cfe3addfcc3' => 
     array (
       0 => 'D:\\Programmes\\laragon\\www\\lbdntools\\templates\\bodyAccueil.tpl',
-      1 => 1614434499,
+      1 => 1614434534,
       2 => 'file',
     ),
   ),
@@ -20,21 +20,23 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_603a50c4124401_97905731 (Smarty_Internal_Template $_smarty_tpl) {
-?><div class=div-content>
-    <h1>Bienvenue sur NormandyRP Tools</h1>
-    <h2>Ici, vous trouverez à votre disposition différents outils.</h2>
-    <ul>
-        <li><img id=inline class=icon src="img/Home_Icon.svg"><span id=inline>Page d'accueil de l'outil.</span></li>
-        <li><img id=inline class=icon src="img/target.svg"><span id=inline>TacticalMap Tool, outil permettant de
-                rajouter des annotations sur des cartes, ainsi l'élaboration de strategies défensives et offensives
-                est facilitée.</span></li>
-        <li><img id=inline class=icon src="img/bomb-explosion-svgrepo-com.svg"><span id=inline>Mortar Tool, outil
-                permettant le calcul de trajectoire des tirs de mortier.</span></li>
-        <li><img id=inline class=icon src="img/write-svgrepo-com.svg"><span id=inline>Outil permettant de rédiger
-                des rapports selon une charte graphique se rapprochant au mieux des documents d'époque.</span></li>
-    </ul>
-</div>
+function content_603a512b322136_61938345 (Smarty_Internal_Template $_smarty_tpl) {
+?><section class="accueil-section">
+    <div class=div-content>
+        <h1>Bienvenue sur NormandyRP Tools</h1>
+        <h2>Ici, vous trouverez à votre disposition différents outils.</h2>
+        <ul>
+            <li><img id=inline class=icon src="img/Home_Icon.svg"><span id=inline>Page d'accueil de l'outil.</span></li>
+            <li><img id=inline class=icon src="img/target.svg"><span id=inline>TacticalMap Tool, outil permettant de
+                    rajouter des annotations sur des cartes, ainsi l'élaboration de strategies défensives et offensives
+                    est facilitée.</span></li>
+            <li><img id=inline class=icon src="img/bomb-explosion-svgrepo-com.svg"><span id=inline>Mortar Tool, outil
+                    permettant le calcul de trajectoire des tirs de mortier.</span></li>
+            <li><img id=inline class=icon src="img/write-svgrepo-com.svg"><span id=inline>Outil permettant de rédiger
+                    des rapports selon une charte graphique se rapprochant au mieux des documents d'époque.</span></li>
+        </ul>
+    </div>
+</section>
 
 <?php }
 }
